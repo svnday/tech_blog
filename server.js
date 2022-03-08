9ee1e0a0db05e1ccca64505c06095b5bfe1d09dc
svnday <40030806+svnday@users.noreply.github.com>
2022-03-07 21:45:19 -0600

    created homepage, login, sign up, post dashboard, post editor, individual post views, comments, and all front-end logic to handlebars

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 const app = express();
